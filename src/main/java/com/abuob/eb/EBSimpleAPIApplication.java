8648b249e9202e0ed4e31348d56bc14f44cf5186
Alex Buob <alexander.buob@gmail.com>
2017-08-18 16:27:02 -0500

    initial commit

---
+++ src/main/java/com/abuob/eb/EBSimpleAPIApplication.java
@@ -0,0 +1,12 @@
+package com.abuob.eb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBSimpleAPIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBSimpleAPIApplication.class, args);
+    }
+}
