2a9a8351f9baf1da09046fb273ce6451fb419a52
Alex Buob <alexander.buob@gmail.com>
2017-08-18 23:39:56 -0500

    initial commit

---
+++ src/main/java/com/abuob/eb/web/TopicListResponse.java
@@ -0,0 +1,28 @@
+package com.abuob.eb.web;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@JacksonXmlRootElement(localName = "topic-list")
+public class TopicListResponse {
+
+    @JacksonXmlProperty(localName = "topicid")
+    @JacksonXmlElementWrapper(useWrapping = false)
+    private List<Long> topicIdList;
+
+    public TopicListResponse() {
+        this.topicIdList= new ArrayList<>();
+    }
+
+    public boolean addTopicId(long topicId) {
+        return topicIdList.add(topicId);
+    }
+
+    public List<Long> getTopicIdList() {
+        return topicIdList;
+    }
+}
