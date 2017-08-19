d786d20fd2bd91baed8fe1216e94df11ee88c938
Alex Buob <alexander.buob@gmail.com>
2017-08-18 23:03:33 -0500

    initial commit

---
+++ src/main/java/com/abuob/eb/enums/ClassEnum.java
@@ -0,0 +1,34 @@
+package com.abuob.eb.enums;
+
+public enum ClassEnum {
+
+    ANIMAL("animal"),
+    ART("art"),
+    BIOGRAPHY("biography"),
+    EVENT("event"),
+    PLACE("place"),
+    PLANT("plant"),
+    SCIENCE("science"),
+    SPORTS("sports"),
+    TECHNOLOGY("TECHNOLOGY"),
+    TOPIC("topic");
+
+    private String value;
+
+    ClassEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static ClassEnum findByValue(String value) {
+        for (ClassEnum classEnum : values()) {
+            if (classEnum.getValue()== value) {
+                return classEnum;
+            }
+        }
+        throw new IllegalArgumentException("Unable to find class:" + value);
+    }
+}
