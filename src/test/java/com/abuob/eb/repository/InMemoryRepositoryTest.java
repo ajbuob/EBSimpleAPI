93028cc48d4a52e936711c4c645ecbb92cf8cca1
Alex Buob <alexander.buob@gmail.com>
2017-08-20 15:03:38 -0500

    initial commit

---
+++ src/test/java/com/abuob/eb/repository/InMemoryRepositoryTest.java
@@ -0,0 +1,23 @@
+package com.abuob.eb.repository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("dev")
+public class InMemoryRepositoryTest {
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    @Test
+    public void someTest() { }
+}
