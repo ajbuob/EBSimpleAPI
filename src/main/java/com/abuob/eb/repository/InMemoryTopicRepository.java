754b9936a3cae9708e62cb77a1e103fd7dc111f6
Alex Buob <alexander.buob@gmail.com>
2017-08-20 15:00:53 -0500

    repository methods implemented

--- src/main/java/com/abuob/eb/repository/InMemoryTopicRepository.java
+++ src/main/java/com/abuob/eb/repository/InMemoryTopicRepository.java
@@ -1,25 +1,48 @@
 package com.abuob.eb.repository;
 
 import com.abuob.eb.service.TopicDTO;
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Repository
+@Profile("dev")
 public class InMemoryTopicRepository implements TopicRepository {
+
+    private final List<TopicDTO> topicDTOList;
+
+    @Value("${eb.testXml.path}")
+    private String xmlTestFilePath;
+
+    public InMemoryTopicRepository() {
+        topicDTOList = Lists.newArrayList();
+    }
+
+    public InMemoryTopicRepository(List<TopicDTO> topicDTOList) {
+        this.topicDTOList = topicDTOList;
+    }
 
     @Override
     public TopicDTO findTopicInfoById(long id) {
-        return null;
+        return topicDTOList.stream()
+                .filter(topic -> Objects.equals(topic.getTopicId(), id)).findFirst().orElse(null);
     }
 
     @Override
     public List<TopicDTO> findAllTopicInfo() {
-        return null;
+        return topicDTOList;
     }
 
     @Override
     public List<Long> findTopicIdsByClass(String className) {
-        return null;
+        return topicDTOList.stream()
+                .filter(topic -> Objects.equals(topic.getUrlClass(), className))
+                .map(TopicDTO::getTopicId)
+                .collect(Collectors.toList());
     }
 }
